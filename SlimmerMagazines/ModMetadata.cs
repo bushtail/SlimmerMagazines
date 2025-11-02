47b18551476b7bef9d55f98a974543d8d419acc8
bushtail <evannosich@icloud.com>
2025-11-02 16:57:17 -0500

    WTT compat

--- SlimmerMagazines/ModMetadata.cs
+++ SlimmerMagazines/ModMetadata.cs
@@ -10,7 +10,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "SlimmerMagazines";
     public override string Author { get; init; } = "bushtail";
     public override List<string>? Contributors { get; init; }
-    public override Version Version { get; init; } = new("1.0.5");
+    public override Version Version { get; init; } = new("1.0.6");
     public override Range SptVersion { get; init; } = new("~4.0.0");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, Range>? ModDependencies { get; init; }
